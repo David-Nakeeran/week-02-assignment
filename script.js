95c7c7409ab0a0f5ede259f59ce45a6673d1cfea
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-09 11:51:01 +0100

    Added arr of imgs

---
+++ script.js
@@ -0,0 +1,29 @@
+const imgs = [
+  {
+    src: "./assets/images/landscape1.png",
+    alt: "Beautiful image of iceberg",
+  },
+  {
+    src: "./assets/images/landscape2.jpg",
+    alt: "Beautiful image of the nights sky",
+  },
+  {
+    src: "./assets/images/landscape3.jpg",
+    alt: "Overlooking lake with backdrop of snowy mountains",
+  },
+  {
+    src: "./assets/images/landscape4.jpg",
+    alt: "Iceberg with backdrop of mounts in the distance",
+  },
+  {
+    src: "./assets/images/landscape5.jpg",
+    alt: "Close up of iceberg, that looks like it's standing out of the water",
+  },
+  {
+    src: "./assets/images/landscape6.jpg",
+    alt: "Block of ice that has a bluey tinge of colour",
+  },
+];
+
+const imgContainer = document.getElementById("img-container");
+const carouselContainer = document.getElementById("thumbnail-container");
