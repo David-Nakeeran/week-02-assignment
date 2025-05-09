4a3a52066eb221cc6ccfdf8452b864e3003aa87c
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-09 14:02:07 +0100

    Added class to large img

--- script.js
+++ script.js
@@ -27,3 +27,51 @@ const imgs = [
 
 const imgContainer = document.getElementById("img-container");
 const carouselContainer = document.getElementById("thumbnail-container");
+
+// generate thumbnail images
+const generateThumbnails = () => {
+  imgs.forEach((ele, index) => {
+    const img = document.createElement("img");
+    img.src = ele.src;
+    img.alt = ele.alt;
+    img.ariaLabel = ele.alt;
+    img.tabIndex = "0";
+    img.setAttribute("id", index);
+    carouselContainer.appendChild(img);
+  });
+};
+
+// create large image tag
+const createLargeImage = () => {
+  const img = document.createElement("img");
+  const backImg = imgs[0].src;
+  img.src = imgs[0].src;
+  img.alt = imgs[0].alt;
+  img.ariaLabel = imgs[0].alt;
+  img.setAttribute("id", "large-img");
+  img.setAttribute("class", "large-image");
+  img.setAttribute("data-index", "0-l");
+  imgContainer.appendChild(img);
+};
+
+// callbacks
+const handleCarouselClick = (ele, index, e) => {
+  const largeImg = document.getElementById("large-img");
+  if (index === parseInt(e.target.id)) {
+    largeImg.src = ele.src;
+    largeImg.alt = ele.alt;
+    largeImg.ariaLabel = ele.alt;
+    largeImg.role = "button";
+    largeImg.setAttribute("data-index", `${index}-l`);
+  }
+};
+
+// loop through array, add event listener to carouselContainer
+imgs.forEach((ele, index) => {
+  carouselContainer.addEventListener("click", (e) => {
+    handleCarouselClick(ele, index, e);
+  });
+});
+
+createLargeImage();
+generateThumbnails();
